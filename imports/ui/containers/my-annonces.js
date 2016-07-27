e3212b574f15a831b049ab45153da4c570d44cb4
ayoub <ayoub.belghar@gail.com>
2016-07-27 03:08:08 +0100

    sort annonce by publication date

--- imports/ui/containers/my-annonces.js
+++ imports/ui/containers/my-annonces.js
@@ -4,10 +4,10 @@ import { AnnoncesList } from "../components/annonces-list.js";
 import { Loading } from '../components/loading.js';
 import { Meteor } from 'meteor/meteor';
 
-const composer = (params, onData) => {	
+const composer = (params, onData) => {
 	const subscriptions = Meteor.subscribe('my-annonces');
 	if(subscriptions.ready()) {
-		const annonces = Annonces.find().fetch();
+		const annonces = Annonces.find({}, {sort:{ publication: -1 }}).fetch();
 		onData(null, { annonces });
 	}
 };
