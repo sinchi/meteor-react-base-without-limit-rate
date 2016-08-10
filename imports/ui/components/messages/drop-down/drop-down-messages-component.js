98b7281aacca251dda999dbcaf2b33c0228fefa3
ayoub <ayoub.belghar@gail.com>
2016-08-10 03:57:18 +0100

    drop dow menu message v1

--- imports/ui/components/messages/drop-down/drop-down-messages-component.js
+++ imports/ui/components/messages/drop-down/drop-down-messages-component.js
@@ -10,30 +10,30 @@ export class DropDownMessagesComponent extends React.Component{
 
   constructor(){
     super(...arguments);
-    this.state = {
-      countAllMessage: 0
-    }
+
   }
 
-  getTotalMessagesCountBadge(){
-      if(this.state.countAllMessage > 0)
-        if(this.state.countAllMessage > 23)
+  getTotalMessagesCountBadge(count){
+      if(count > 0)
+        if(count > 23)
           return <Badge pullRight>+23</Badge>
         else
-          return <Badge pullRight>{this.state.countAllMessage}</Badge>
+          return <Badge pullRight>{count}</Badge>
   }
 
   render(){
+          let countAllMessage = 0;
           let friends =  _.map(this.props.friends, (message) => {
           let user = (message.from.userId === Meteor.userId()) ? Meteor.users.findOne({ _id: message.to.userId }, { fields: { profile : 1 } }) : Meteor.users.findOne({ _id: message.from.userId }, { fields: { profile : 1 } });
           let that = this;
           let active = false;
+
           if(!message.to.read && (message.from.userId !== Meteor.userId()))
             active = "active bounceInDown";
           else if(this.props.conversationId === message.conversationId)
             active = true;
           let countReceivedMessageNumber = ConversationMessages.find({ conversationId: message.conversationId, "to.userId": Meteor.userId(), "to.read": false }).count();
-          that.setState({ countAllMessage: that.state.countAllMessage + countReceivedMessageNumber });
+          countAllMessage += countReceivedMessageNumber;
           return {
                 _id: message._id,
                 avatar: user.profile.avatar,
@@ -54,15 +54,11 @@ export class DropDownMessagesComponent extends React.Component{
       eventKey++;
         return <DropDownMessagesMenuItemComponent friend= { friend }/>
     });
-    //     return <MenuItem onSelect={this.props.makeRead}  eventKey={ category._id }><Icon name={ this.getIconNotification(category) } size="lg" pullLeft/> { category.name } {this.getCategoryNotification(category)}</MenuItem>;
-    // });
-    //
-    // let menuItems = (this.props.categories && this.props.categories.length > 0) ? items : (<MenuItem eventKey={ 2.2 } > Vous n êtes abonné à aucune catégorie !</MenuItem>);
 
     return (
       <NavDropdown
         eventKey={ 2 }
-        title={<span>  Messages { this.getTotalMessagesCountBadge() }</span> }
+        title={<span>  Messages { this.getTotalMessagesCountBadge(countAllMessage ) } </span> }
         id="basic-nav-dropdown">
         <MenuItem eventKey={ 2.1 } ><Icon name="plus-circle" size="lg" /> Afficher Tous </MenuItem>
         <MenuItem divider />
